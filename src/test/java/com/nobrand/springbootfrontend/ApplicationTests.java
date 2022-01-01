f108f171ddd12c928e2de89b02013e4ab9048f8e
no-brand <do.dream.david@gmail.com>
2022-01-02 01:24:16 +0900

    GET /hello (test with JUnit4)

--- src/test/java/com/nobrand/springbootfrontend/ApplicationTests.java
+++ src/test/java/com/nobrand/springbootfrontend/ApplicationTests.java
@@ -1,14 +1,33 @@
 package com.nobrand.springbootfrontend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@WebMvcTest
 class ApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void hello() throws Exception {
+		String hello = "hello";
+
+		mvc.perform(MockMvcRequestBuilders.get("/hello"))
+		   .andExpect(MockMvcResultMatchers.status().isOk())
+		   .andExpect(MockMvcResultMatchers.content().string(hello));
 	}
 
 }
